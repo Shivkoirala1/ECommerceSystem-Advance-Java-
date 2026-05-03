810acd49f2d814764866098ba0f2ef10349302be
Shivkoirala1 <koirala.khushi456@gmail.com>
2026-05-03 22:25:30 +0545

    LogoutServlet.java Servlet

--- src/main/java/com/ecommerce/controller/LogoutServlet.java
+++ src/main/java/com/ecommerce/controller/LogoutServlet.java
@@ -1,4 +1,19 @@
 package com.ecommerce.controller;
 
-public class LogoutServlet {
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        HttpSession session = req.getSession(false);
+        if (session != null) session.invalidate();
+        Cookie cookie = new Cookie("username", "");
+        cookie.setMaxAge(0);
+        cookie.setPath("/");
+        resp.addCookie(cookie);
+        resp.sendRedirect(req.getContextPath() + "/login");
+    }
 }
